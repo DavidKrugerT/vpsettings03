24704cd83acc046799b17338fa965f11690a089a
David Krüger <david.tropakruger@gmail.com>
2018-05-11 13:55:49 +0200

    allt är nytt

---
+++ app/src/main/java/com/example/david/vpsettings03/DefaultSettings.java
@@ -0,0 +1,4 @@
+package com.example.david.vpsettings03;
+
+public class DefaultSettings {
+}
--- app/src/main/java/com/example/david/vpsettings03/MainActivity.java
+++ app/src/main/java/com/example/david/vpsettings03/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.david.vpsettings03;
 
+import android.annotation.TargetApi;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +20,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        };
+
+        Fragment fragment = new SettingsScreen();
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        if (savedInstanceState == null) {
+            //created for the first time
+            fragmentTransaction.add(R.id.relative_layout, fragment, "settings_fragment");
+            fragmentTransaction.commit();
+        } else {
+            fragment = getFragmentManager().findFragmentByTag("settings_fragment");
+        }
     }
+
+    public static class SettingsScreen extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_screen);
+            SwitchPreference switchPreference = (SwitchPreference) findPreference("switch_active");
+            switchPreference.setEnabled(true);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class GeneralPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_screen);
+            setHasOptionsMenu(true);
+
+            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
+            // to their values. When their values change, their summaries are
+            // updated to reflect the new value, per the Android Design
+            // guidelines.
+            bindPreferenceSummaryToValue(findPreference("user_name"));
+            bindPreferenceSummaryToValue(findPreference("user_email"));
+        }
+
+        private void bindPreferenceSummaryToValue(Preference example_text) {
+        }
+
+    }
+
+}
 
